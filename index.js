f37487de7ba729784ab95f6ac7e765e7cfb7464b
Julio Sanjuan <julio.sanjuan@dacodes.com.mx>
2023-07-05 18:10:00 -0600

    added tick timer

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello World!")
+const moment = require("moment")
+
+/**
+ * This is a function that prints "tick" to the console
+ */
+const tick = () => {
+    const now = moment().format("h:mm:ss a")
+    console.clear()
+    console.log("it's ", now)
+}
+
+/**
+ * Init an interval that calls the tick function every second
+ */
+setInterval(tick, 1000)
